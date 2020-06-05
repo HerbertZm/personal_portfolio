9485fa4a7d3f8c64e9d0e73a729fb0c9798f6a85
Herbert Zurita <herbo351@gmail.com>
2020-06-05 00:39:03 -0500

    no bootstrap

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'portfolio_app',
 ]
 
@@ -120,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_FILES = [ ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
